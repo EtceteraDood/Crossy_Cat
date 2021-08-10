c731073b9030d75a7e49d728740ca73674cedb3a
Valdemar Etcetera <EtceteraDood@users.noreply.github.com>
2021-08-10 15:42:25 -0700

    Put source code into function and classes

--- Crossy_Cat.py
+++ Crossy_Cat.py
@@ -1,27 +1,40 @@
 import pygame
 
-pygame.init()
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 800
 GAME_TITLE = "Crossy Cat"
+# Adjusted size 800x800 standard
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
 WHITE_BACKGROUNG = (255, 255, 255)
 clock = pygame.time.Clock()
-FPS = 60
-is_gameover = False
 
-game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-game_screen.fill(WHITE_BACKGROUNG)
-pygame.display.set_caption(GAME_TITLE)
+class Game:
+    FPS = 60
 
-# Main game loop
-while not is_gameover:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: is_gameover = True
-    print(event)  # Console event logger (Can be deleted)
+    def __init__(self, title, width, height):
+        self.title = title
+        self.width = width
+        self.height = height
 
-    pygame.display.update()  # Update all game graphics
-    clock.tick(FPS)  # Updates everything within the game
+        game_screen = pygame.display.set_mode((width, height))
+        game_screen.fill(WHITE_BACKGROUNG)
+        pygame.display.set_caption(title)
+
+    def run_game(self):
+        is_gameover = False
+
+        # Main game loop
+        while not is_gameover:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: is_gameover = True
+            print(event)  # Console event logger (Can be deleted)
+
+            pygame.display.update()  # Update all game graphics
+            clock.tick(self.FPS)  # Updates everything within the game
+
+pygame.init()
+
+new_game = Game(GAME_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT)
+new_game.run_game()
 
 # Quit pygame and the program
 pygame.quit()
